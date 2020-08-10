47b81c8ece01df89cd7f02b6a889f43864b76580
Ankita Priya <apriya@Ankitas-MacBook-Pro.local>
2020-08-10 22:15:54 +0530

    [LinkList has cycle].

---
+++ src/main/java/solutions/LinkListHasCycle.java
@@ -0,0 +1,24 @@
+package solutions;
+
+import utility.ListNode;
+
+/*
+https://leetcode.com/problems/linked-list-cycle/
+ */
+
+public class LinkListHasCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode oneStep = head;
+        ListNode twoSteps = head;
+        oneStep = oneStep != null ? oneStep.next : null;
+        twoSteps = twoSteps != null && twoSteps.next != null ? twoSteps.next.next : null;
+        while (oneStep != null && twoSteps != null) {
+            if (oneStep == twoSteps) {
+                return true;
+            }
+            oneStep = oneStep.next;
+            twoSteps = twoSteps.next != null ? twoSteps.next.next : null;
+        }
+        return false;
+    }
+}
--- src/main/java/solutions/ReverseLinkList.java
+++ src/main/java/solutions/ReverseLinkList.java
@@ -1,3 +1,5 @@
+package solutions;
+
 import utility.ListNode;
 
 
